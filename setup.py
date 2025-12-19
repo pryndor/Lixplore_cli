e99c67553b3591de358dd23713c482cf4d04107a
Bala-periannan <balathepharmacist@gmail.com>
2025-12-19 19:59:46 +0530

    Your update

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
             'lixplore=lixplore.cli:main',
         ],
     },
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     keywords='literature search academic pubmed arxiv crossref research papers export bibliography',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -45,7 +45,6 @@ setup(
         'Topic :: Text Processing :: General',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
